649681184b9cd30c041fbb8599d519f5f2ea2e3d
Andrii Deputovych <a.deputovich@gmail.com>
2023-05-05 18:28:49 +0300

    feat: open browser when UI runing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +7,12 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `$injectedColor: orange;`,
-      }
-    }
+      },
+    },
   },
   plugins: [react()],
-})
+  server: {
+    open: true,
+    port: 1212,
+  },
+});
