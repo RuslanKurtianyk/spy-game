89546f9b9b88e5a609b0834c86da4f5959645aed
ruslan <ruslan.kurtianyk@gossc.com>
2023-05-05 13:59:07 +0300

    chore: refactor game controller

--- server/src/controllers/game.controller.ts
+++ server/src/controllers/game.controller.ts
@@ -3,20 +3,24 @@ import { createNewGame, deleteGameById, getGamesList, getGameById, updateGame } 
 import { Game } from 'src/entity';
 
 export const GameController = {
-  getList: async (req: Request, res: Response): Promise<void> => {
-    res.send(await getGamesList());
+  getList: async (request: Request, response: Response): Promise<void> => {
+    response.send(await getGamesList());
   },
-  getOne: async (req: Request, res: Response): Promise<void> => {
-    res.send(await getGameById(req.params.id));
+  getOne: async (request: Request, response: Response): Promise<void> => {
+    const randomLocation = await getGameById(request.params.id);
+    const status = randomLocation ? 200 : 404;
+    const result = randomLocation ? randomLocation : 'Game not found';
+    response.status(status).send(result);
+
   },
-  create: async (req: Request<Partial<Game>>, res: Response) => {
-    const result = await createNewGame(req.body);
-    res.send(result);
+  create: async (request: Request<Partial<Game>>, response: Response) => {
+    const result = await createNewGame(request.body);
+    response.send(result);
   },
-  update: async (req: Request, res: Response): Promise<void> => {
-    res.send(await updateGame(req.body));
+  update: async (request: Request, response: Response): Promise<void> => {
+    response.send(await updateGame(request.body));
   },
-  delete: async (req: Request, res: Response): Promise<void> => {
-    res.send(await deleteGameById(req.params.id));
+  delete: async (request: Request, response: Response): Promise<void> => {
+    response.send(await deleteGameById(request.params.id));
   }
 };
