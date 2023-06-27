ecf0875149fc4659f06ee0ee3d0b2121d14f5c8c
ruslan <ruslan.kurtianyk@gossc.com>
2023-06-27 14:14:14 +0300

    fix: edit server swagger file

--- server/src/services/game.service.ts
+++ server/src/services/game.service.ts
@@ -23,7 +23,7 @@ export const getGameById = async (id: string, userName?: string): Promise<Game> 
 
 export const createNewGame = async (game: Partial<Game>) => {
   const gameRepository = appDataSource.getRepository(Game);
-  const entity = gameRepository.create(game);
+  const entity = gameRepository.create({...game, admin: null});
 
   return await gameRepository.save(entity);
 }
